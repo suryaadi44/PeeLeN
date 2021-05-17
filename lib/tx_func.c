4227a7b195497945369bfdbbac2d20376d02788b
suryaadi44 <suryawarior44@gmail.com>
2021-05-18 03:40:28 +0800

    Menambahkan fungsi viewUsage, addUsage dan payUsage pada tx_func

---
+++ lib/tx_func.c
@@ -0,0 +1,145 @@
+#include "tx_func.h"
+
+void viewUsage() {
+    FILE* database, * config;
+    customer cust;
+    rate rates;
+
+    long id;
+    int max = countLine(CUST_DB_PATH);
+    int configMax = countLine(RATE_PATH);
+
+    config = fopen(RATE_PATH, "r");
+    database = fopen(CUST_DB_PATH, "r");
+
+    clear();
+    header();
+    printf(" --> TAMPILKAN DATA PENGGUNAAN \n");
+    printf("==============================================================================\n");
+    printf("                             Masukan ID Pelanggan                             \n");
+    printf("==============================================================================\n");
+    printf(" ID Pelanggan    : ");
+    fflush(stdin);
+    scanf("%ld", &id);
+    printf("==============================================================================\n");
+
+    for (int i = 0; i < max; i++) {
+        fscanf(database, "%ld,%[^,],%[^,],\"%[^\"]\",%[^,],%f,%d\n", &cust.id, cust.namaDepan, cust.namaBelakang, cust.address, cust.telp, &cust.usage, &cust.price);
+        if (cust.id == id)
+            break;
+    }
+
+    for (int i = 0; i < configMax; i++) {
+        fscanf(config, "%d,%d,%d", &rates.id, &rates.limit, &rates.perKWH);
+        if (rates.id == cust.price)
+            break;
+    }
+
+    if (cust.id == id) {
+        printf(" ID          : %ld\n", cust.id);
+        printf(" Nama        : %s %s\n", cust.namaDepan, cust.namaBelakang);
+        printf(" Tarif       : %d\n", cust.price);
+        printf(" Batas Daya  : %d VA\n", rates.limit);
+        printf(" Penggunaan  : %.4f KWH\n", cust.usage);
+        printf(" Tagihan     : Rp.%.2f\n", cust.usage * rates.perKWH);
+    }
+    printf("==============================================================================\n");
+
+    fclose(config);
+    fclose(database);
+}
+
+void addUsage() {
+    FILE* database, * tempFile;
+    customer cust;
+
+    long id;
+    float add;
+    int max = countLine(CUST_DB_PATH);
+
+    database = fopen(CUST_DB_PATH, "r");
+    tempFile = fopen(TEMP_CUST_DB_PATH, "a");
+
+    clear();
+    header();
+    printf(" --> TAMBAH PENGGUNAAN \n");
+    printf("==============================================================================\n");
+    printf("                             Masukan ID Pelanggan                             \n");
+    printf("==============================================================================\n");
+    printf(" ID Pelanggan    : ");
+    fflush(stdin);
+    scanf("%ld", &id);
+    printf("==============================================================================\n");
+    printf(" Penggunaan      : ");
+    fflush(stdin);
+    scanf("%f", &add);
+    printf("==============================================================================\n");
+
+    for (int i = 0; i < max; i++) {
+        fscanf(database, "%ld,%[^,],%[^,],\"%[^\"]\",%[^,],%f,%d\n", &cust.id, cust.namaDepan, cust.namaBelakang, cust.address, cust.telp, &cust.usage, &cust.price);
+        if (cust.id == id)
+            cust.usage += add;
+        fprintf(tempFile, "%ld,%s,%s,\"%s\",%s,%f,%d\n", cust.id, cust.namaDepan, cust.namaBelakang, cust.address, cust.telp, cust.usage, cust.price);
+    }
+
+
+    fclose(database);
+    fclose(tempFile);
+
+    remove(CUST_DB_PATH);
+    rename(TEMP_CUST_DB_PATH, CUST_DB_PATH);
+}
+
+void payUsage() {
+    FILE* database, * tempFile, * config;
+    customer cust;
+    rate rates;
+
+    long id;
+    int pay;
+    int max = countLine(CUST_DB_PATH);
+    int configMax = countLine(RATE_PATH);
+
+    config = fopen(RATE_PATH, "r");
+    database = fopen(CUST_DB_PATH, "r");
+    tempFile = fopen(TEMP_CUST_DB_PATH, "a");
+
+    clear();
+    header();
+    printf(" --> TAMBAH PENGGUNAAN \n");
+    printf("==============================================================================\n");
+    printf("                             Masukan ID Pelanggan                             \n");
+    printf("==============================================================================\n");
+    printf(" ID Pelanggan      : ");
+    fflush(stdin);
+    scanf("%ld", &id);
+    printf("==============================================================================\n");
+    printf(" Jumlah Pembayaran : ");
+    fflush(stdin);
+    scanf("%d", &pay);
+    printf("==============================================================================\n");
+
+    for (int i = 0; i < max; i++) {
+        fscanf(database, "%ld,%[^,],%[^,],\"%[^\"]\",%[^,],%f,%d\n", &cust.id, cust.namaDepan, cust.namaBelakang, cust.address, cust.telp, &cust.usage, &cust.price);
+        if (cust.id == id) {
+            for (int i = 0; i < configMax; i++) {
+                fscanf(config, "%d,%d,%d", &rates.id, &rates.limit, &rates.perKWH);
+                if (rates.id == cust.price)
+                    break;
+            }
+            cust.usage -= ((float)pay / (float)rates.perKWH);
+        }
+        fprintf(tempFile, "%ld,%s,%s,\"%s\",%s,%f,%d\n", cust.id, cust.namaDepan, cust.namaBelakang, cust.address, cust.telp, cust.usage, cust.price);
+    }
+
+    fclose(config);
+    fclose(database);
+    fclose(tempFile);
+
+    remove(CUST_DB_PATH);
+    rename(TEMP_CUST_DB_PATH, CUST_DB_PATH);
+}
+
+void logTX() {
+
+}
---
+++ lib/tx_func.h
@@ -0,0 +1,11 @@
+#ifndef TX_FUNC_H_
+#define TX_FUNC_H_
+
+#include "mainLogic.h"
+
+void viewUsage();
+void addUsage();
+void payUsage();
+void logTX();
+
+#endif
